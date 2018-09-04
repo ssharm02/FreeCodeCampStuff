85dd12985680a3307ef42f6e34e2bafcc45eab03
Sarthak <sarthak.sharma02@gmail.com>
2018-09-04 19:27:47 -0400

    finished basic data structs fcc

--- Javascript Algorithms and Data Structures Certification/Basic Data Structures/basicDataStructures.js
+++ Javascript Algorithms and Data Structures Certification/Basic Data Structures/basicDataStructures.js
@@ -245,3 +245,60 @@ return false;
 }
 
 console.log(isEveryoneHere(users));
+
+let users = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // change code below this line
+return Object.keys(obj);
+  // change code above this line
+}
+
+console.log(getArrayOfUsers(users));
+
+//modify array stored in an object
+let user = {
+  name: 'Kenneth',
+  age: 28,
+  data: {
+    username: 'kennethCodesAllDay',
+    joinDate: 'March 26, 2016',
+    organization: 'freeCodeCamp',
+    friends: [
+      'Sam',
+      'Kira',
+      'Tomo'
+    ],
+    location: {
+      city: 'San Francisco',
+      state: 'CA',
+      country: 'USA'
+    }
+  }
+};
+
+function addFriend(userObj, friend) {
+  // change code below this line  
+  userObj.data.friends.push(friend);
+  return userObj.data.friends;
+  // change code above this line
+}
+
+console.log(addFriend(user, 'Pete'));

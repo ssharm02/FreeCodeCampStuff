d664041282d1bf812cb3f45ad844049f5733956a
Xcode User <xcode@Fasts-MacBook-Pro-2.local>
2018-11-08 10:10:14 -0500

    es6 stuff

--- Javascript Algorithms and Data Structures Certification/ES6/ES7.js
+++ Javascript Algorithms and Data Structures Certification/ES6/ES7.js
@@ -177,3 +177,17 @@ let a = 8, b = 6;
 })();
 console.log(a); // should be 6
 console.log(b); // should be 8
+const AVG_TEMPERATURES = {
+  today: 77.5,
+  tomorrow: 79
+};
+
+function getTempOfTmrw(avgTemperatures) {
+  "use strict";
+  // change code below this line
+   const { tomorrow: tempOfTomorrow } = avgTemperatures// change this line
+  // change code above this line
+  return tempOfTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES)); // should be 79
